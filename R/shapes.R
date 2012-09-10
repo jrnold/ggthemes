7ba56b4df847ba7f0ca29fb8393cf5fe8a103741
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-09-09 21:42:25 -0400

    added shape scales from Tremmel (1995) and a filled circle scale

--- R/shapes.R
+++ R/shapes.R
@@ -15,6 +15,9 @@
 ##' vertical line. If someone can improve this palette, please let me
 ##' know.
 ##'
+##' Following Tremmel (1995), I replace the cirlce with a vertical line
+##' with an encircled plus sign.
+##'
 ##' @examples
 ##' # xoverlapping symbol palette
 ##' dsamp <- diamonds[sample(nrow(diamonds), 100), ]
@@ -25,8 +28,11 @@
 ##' + theme_bw() + scale_shape_cleveland(overlap=FALSE))
 ##'
 ##' @references
-##' Cleveland WS. The Elements of Graphing Data. Revised Edition. Hobart Press, Summit, NJ, 1994,
-##' pp. 154-164, 234-239.
+##' Cleveland WS. The Elements of Graphing Data. Revised Edition. Hobart Press, Summit, NJ, 1994, pp. 154-164, 234-239.
+##' @references Tremmel, Lothar, (1995) "The Visual Separability of Plotting Symbols in Scatterplots"
+##' Journal of Computational and Graphical Statistics,
+##' \url{http://www.jstor.org/stable/1390760}
+##'
 cleveland_shape_pal <- function(overlap=TRUE) {
     function(n) {
         maxshapes <- 5
@@ -46,11 +52,11 @@ cleveland_shape_pal <- function(overlap=TRUE) {
           83  # W
           )[seq_len(n)]
         } else {
-            c(1,
-              19, ## ⚫ 26AB
-              -0x25CE, ## ◎ 25CE
-              -0x2609, ## ☉ 2609
-              -0x25C9 ## ◉ 25C9
+            c(1, ## empty circle
+              19, ## solid circle
+              10, ## encircled plus sign
+              -0x2299, ## ⊙ 2299
+              -0x229A ## ⊚ 229A
               )[seq_len(n)]
         }
     }
@@ -62,6 +68,7 @@ cleveland_shape_pal <- function(overlap=TRUE) {
 ##' @inheritParams cleveland_shape_pal
 ##' @export
 ##'
+##' @seealso \code{\link{cleveland_shape_pal}} for a description of the palette.
 ##' @references
 ##' Cleveland WS. The Elements of Graphing Data. Revised Edition. Hobart Press, Summit, NJ, 1994,
 ##' pp. 154-164, 234-239.
@@ -95,3 +102,110 @@ scale_shape_cleveland <- function(overlap=TRUE, ...) {
 ## ◷ 25F7
 ## ◯ 25EF
 
+##' Filled Circle Shape palette (discrete)
+##'
+##' Shape palette with circles varying by amount of fill. This uses
+##' the set of 3 circle fill values in Lewandowsky and Spence (1989):
+##' solid, hollow, half-filled, with two additional fill amounts:
+##' three-quarters, and one-quarter.
+##'
+##' @references Lewandowsky, Stephan and Ian Spence (1989)
+##' "Discriminating Strata in Scatterplots", Journal of
+##' the American Statistical Assocation, \url{http://www.jstor.org/stable/2289649}
+circle_fill_pal <- function() {
+    maxshapes <- 5
+    types <- c(16, 1, -0x25D3, -0x25D5, -0x25D4)
+    function (n) {
+        if (n > maxshapes) {
+            msg <- sprintf(paste("The shape palette can deal with a maximum of %d discrete ",
+                                 "values because more than %d becomes difficult to discriminate; ",
+                                 "you have ", n, ". Consider specifying shapes manually. if you ",
+                                 "must have them.", sep = ""),
+                           maxshapes, maxshapes)
+            warning(paste(strwrap(msg), collapse = "\n"), call. = FALSE)
+        }
+        types[seq_len(n)]
+    }
+}
+
+##' Filled Circle Shape palette (discrete)
+##'
+##' @inheritParams ggplot2::discrete_x_scale
+##' @seealso \code{\link{circle_fill_pal}} for a description of the palette.
+scale_shape_fill <- function(...) {
+    discrete_scale("shape", "fill", circle_fill_pal(), ...)
+}
+
+##' Shape palette from Tremmel (1995) (discrete)
+##'
+##' Based on experiments Tremmel (1995) suggests the following shape palettes:
+##'
+##' If two symbols, then use a solid circle and plus sign.
+##'
+##' If three symbols, then use a solid circle, empty circle, and an
+##' empty triangle. However, that set of symbols does not satisfy the
+##' requirement that each symbol should differ from the other symbols
+##' in the same feature dimension. A set of three symbols that
+##' satisfies this is a cicle (curvature), plus sign (number of
+##' terminators), triangle (line orientation).
+##'
+##' If more than three groups of data, then separate the groups into
+##' different plots.
+##'
+##' @param overlap use an empty circle instead of a solid circle when
+##' \code{n == 2}.
+##' @param n3alt If \code{TRUE} then use a solid circle, plus sign and
+##' empty triangle, else use a solid circle, empty circle, and empty
+##' triangle.
+##' @references Tremmel, Lothar, (1995) "The Visual Separability of Plotting Symbols in Scatterplots"
+##' Journal of Computational and Graphical Statistics,
+##' \url{http://www.jstor.org/stable/1390760}
+tremmel_shape_pal <- function(overlap=FALSE, n3alt=TRUE) {
+    maxshapes <- 3
+    function (n) {
+        if (n == 1)  {
+            16 # solid circle
+        } else if (n == 2) {
+            c(ifelse(overlap, 1, 16), # hollow or solid circle
+              3) # plus sign
+        } else if (n == 3) {
+            if (n3alt) {
+                c(16, # solid circle,
+                  3, # plus
+                  2) # hollow triangle
+            } else {
+                c(16, # solid circle,
+                  1, # hollow circle
+                  2) # triangle
+            }
+        } else if (n > maxshapes) {
+            msg <- sprintf(paste("The shape palette can deal with a maximum of %d discrete ",
+                                 "values because more than %d becomes difficult to discriminate; ",
+                                 "you have ", n, ". Consider specifying shapes manually. if you ",
+                                 "must have them.", sep = ""),
+                           maxshapes, maxshapes)
+            warning(paste(strwrap(msg), collapse = "\n"), call. = FALSE)
+        }
+    }
+}
+
+
+##' Shape scales from Tremmel (1995)
+##'
+##' @inheritParams ggplot2::scale_x_discrete
+##' @inheritParams tremmel_shape_pal
+##' @export
+##'
+##' @seealso \code{\link{tremmel_shape_pal}} for a description of the palette.
+##' @examples
+##' (ggplot(mtcars, aes(x=mpg, y=hp, shape=factor(cyl)))
+##'  + geom_point() + scale_shape_tremmel())
+##' (ggplot(mtcars, aes(x=mpg, y=hp, shape=factor(cyl)))
+##'  + geom_point() + scale_shape_tremmel(n3alt=FALSE))
+##' (ggplot(mtcars, aes(x=mpg, y=hp, shape=factor(am)))
+##'  + geom_point() + scale_shape_tremmel())
+##' (ggplot(mtcars, aes(x=mpg, y=hp, shape=factor(am)))
+##'  + geom_point() scale_shape_tremmel(overlap=TRUE))
+scale_shape_tremmel <- function(overlap=FALSE, n3alt=TRUE, ...) {
+    discrete_scale("shape", "tremmel", tremmel_shape_pal(overlap=overlap, n3alt=n3alt), ...)
+}
