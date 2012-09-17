ccac3b02d1d1913d6be7b4e8f459a4ffeb9595c7
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-09-16 20:08:28 -0400

    bumped version, changed to GPL-2, added package description

---
+++ R/ggplotJrnold-package.R
@@ -0,0 +1,8 @@
+##' ggplotJrnold
+##'
+##' This package contains extra miscellaneous themes, scales, and
+##' geoms, and function for and related to \bold{ggplot2}.
+##'
+##' @name ggplotJrnold
+##' @docType package
+NULL
