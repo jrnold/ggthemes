cb045d825baead9cd9c9dca14907b1c3935d01a0
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-09-20 18:01:37 -0400

    misc

--- R/utils.R
+++ R/utils.R
@@ -5,12 +5,15 @@
 ##' @export
 ##' @param shapes A numeric or character vector of shapes. See
 ##' \code{\link{par}}.
+##' @param labels Include the plotting character value of the symbol.
 ##' @seealso \code{\link[scales]{show_col}}, \code{\link{show_linetypes}}
 ##'
 ##' @examples
 ##' library(scales)
 ##' show_shapes(shape_pal()(5))
-show_shapes <- function(shapes) {
+##' show_shapes(shape_pal()(3), labels=TRUE)
+##'
+show_shapes <- function(shapes, labels=TRUE) {
     n <- length(shapes)
     ncol <- ceiling(sqrt(n))
     nrow <- ceiling(n/ncol)
@@ -22,6 +25,9 @@ show_shapes <- function(shapes) {
     for (i in seq_len(ncol(shapes))) {
         for (j in seq_len(nrow(shapes))) {
             points(i, j, pch=shapes[j, i])
+            if (labels) {
+                text(i, j, shapes[j, i], pos=1, col="gray70")
+            }
         }
     }
 }
@@ -33,19 +39,26 @@ show_shapes <- function(shapes) {
 ##' @export
 ##' @param linetypes A character vector of linetypes. See
 ##' \code{\link{par}}.
+##' @param labels Label each line with its linetype (lty) value.
+##'
 ##' @seealso \code{\link[scales]{show_col}}, \code{\link{show_linetypes}}
 ##'
 ##' @examples
 ##' library(scales)
-##' show_shapes(linetype_pal()(3))
-show_linetypes <- function(linetypes) {
+##' show_linetypes(linetype_pal()(3))
+##' show_linetypes(linetype_pal()(3), labels=TRUE)
+show_linetypes <- function(linetypes, labels=TRUE) {
     n <- length(linetypes)
     plot(0, 0, xlim=c(0, 1), ylim=c(n, 1),
          type="n", xlab="", ylab="", axes=FALSE)
     for (i in seq_along(linetypes)) {
         abline(h=i, lty=linetypes[i])
     }
-    axis(side=2, at=seq_len(n), tick=FALSE, labels=seq_len(n), las=2)
+    if (labels) {
+        axis(side=2, at=seq_len(n), tick=FALSE, labels=linetypes, las=2)
+    } else {
+        axis(side=2, at=seq_len(n), tick=FALSE, labels=seq_len(n), las=2)
+    }
 }
 
 ## altered from same function from ggplot2
