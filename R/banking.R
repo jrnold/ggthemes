7e6f67733833b789782ca745c89cefe6a1f938ab
Jeff Arnold <jeffrey.arnold@gmail.com>
2012-09-20 22:53:17 -0400

    cleaning up banking to 45 code

--- R/banking.R
+++ R/banking.R
@@ -1,16 +1,6 @@
 ## 45 degrees in radians
 FORTY_FIVE <- 45 * pi / 180
 
-##' Calculate slopes
-##'
-##' @param x x values
-##' @param y y values
-##' @param cull \code{logical}. Remove all slopes of 0 or \code{Inf}.
-##'
-##' @return Vector of slopes of line segments drawn between the
-##' points. The vector has the attribute "lengths", with the length of
-##' each segment.
-##'
 slopes <- function(x, y, cull=FALSE) {
     dx <- diff(x)
     dy <- diff(y)
@@ -21,27 +11,108 @@ slopes <- function(x, y, cull=FALSE) {
         s <- s[touse]
         w <- s[touse]
     }
-    attr(s, "lengths") <- w
-    s
+    list(s = s, lengths = w,
+         range_x = range(x),
+         range_y = range(y))
 }
 
+##' Bank Slopes to 45 degrees
+##'
+##' Calculate the optimal aspect ratio of a line graph by banking the
+##' slopes to 45 degrees as suggested by W.S. Cleveland. This
+##' maximizes the ability to visually differentiate differences in
+##' slope.
+##'
+##' @param x x values
+##' @param y y values
+##' @param cull \code{logical}. Remove all slopes of 0 or \code{Inf}.
+##' @param weight \code{logical}. Weight line segments by their
+##' length. Only used when \code{method="ao"}.
+##' @param method One of "ms" (Median Absolute Slope), "as" (Average
+##' Absolute Slope), "ao" (Average Orientation), "lor" (Local
+##' Orientation Resolution), "gor" (Local Orientation Resolution).
+##' @param ... Passed to \code{\link{nlm}} in methods "so", "lor" and
+##' "gor".
+##'
+##' @note
+##'
+##' This function calculates the optimal aspect ratio for a line plot
+##' using any of the methods described in Herr and Argwala (200?),
+##' many of which were developed by W.S. Cleveland. In their review of
+##' the methods they suggest using median absolute slope banking
+##' ("ms"), which produces aspect ratios which are generally the
+##' median of the various methods.
+##'
 ##' Median Absolute Slope Banking
 ##'
-##' Let the aspect ratio be \deqn{\alpha = \frac{w}{h}}{alpha = w/h}.
+##' Let the aspect ratio be \eqn{\alpha = \frac{w}{h}}
 ##' then the median absolute slop banking is the
-##' \deqn{\alpha}{alpha} such that,
-##' \eqn{
+##' \eqn{\alpha} such that,
+##' \deqn{
 ##'   median \left| \frac{s_i}{\alpha} \right| = 1
 ##' }{
 ##'  median |s_i / alpha|
 ##' }
 ##'
-##' Let \deqn{R_z = z_{max} - z_{min}} for \deqn{z = x, y},
+##' Let \eqn{R_z = z_{max} - z_{min}} for \eqn{z = x, y},
 ##' and \eqn{M = median \| s_i \|}. Then,
-##' \eqn{
+##' \deqn{
 ##' \alpha = M R_x / R_y
 ##' }
 ##'
+##' Average-Absolute-Orientation Banking
+##'
+##' Find the aspect ratio by setting the average orientation to 45
+##' degrees. For aspect ratio \eqn{\alpha}{alpha}, let the
+##' orientation of a line segment be \eqn{\theta_i(\alpha) = atan(s_i
+##' / \alpha)}.
+##'
+##' \deqn{
+##' \frac{ \sum_i \theta_i(\alpha) l_i}{\sum_i l_i} = \frac{\pi}{4} rad
+##' }
+##' where \eqn{l_i = 1} if unweighted, and \eqn{l_i = \sqrt{x_i^2 + y_i^2}}
+##' (length of the line segment), if weighted.
+##' The value of \eqn{\alpha} is found with \code{\link{nlm}}.
+##'
+##' Average Absolute Slope Banking
+##'
+##' Let the aspect ratio be \eqn{\alpha = \frac{w}{h}}{alpha = w/h}.
+##' then the mean absolute slope banking is the
+##' \eqn{\alpha}{alpha} such that,
+##' \deqn{
+##'   mean \left| \frac{s_i}{\alpha} \right| = 1
+##' }{
+##'  mean |s_i / alpha| = 1
+##' }
+##'
+##' Let \eqn{R_z = z_{max} - z_{min}} for \eqn{z = x, y},
+##' and \eqn{M = mean \| s_i \|}. Then,
+##' \deqn{
+##' \alpha = M R_x / R_y
+##' }
+##'
+##' Banking by Optimizing Orientation Resolution
+##'
+##' The angle between line segments i and j is
+##' \eqn{r_{i,j} = \|\theta_i(\alpha) - \theta_j(\alpha)\|}
+##'
+##' where \eqn{\theta_i(\alpha) = atan(s_i /  \alpha)} and
+##' \eqn{s_i} is the slope of line segment i.
+##' This function finds the  \eqn{\alpha} that maximizes
+##' the sum of the angles between all pairs of line segments.
+##'
+##' \deqn{
+##'   \max_{\alpha} \sum_i \sum_{j: j < 1} r_{i,j}
+##' }
+##'
+##' The local optimization only includes line-segments
+##' that are next to each other. Suppose there are n line
+##' segments, then the local orientation orientation
+##' has the following objective function.
+##'
+##' \deqn{
+##'   \max_{\alpha} \sum_{i=2}^{n} r_{i,i-1}
+##' }
 ##'
 ##' @references
 ##' Cleveland, W. S., M. E. McGill, and R. McGill. The Shape Parameter
@@ -51,128 +122,86 @@ slopes <- function(x, y, cull=FALSE) {
 ##' Heer, Jeffrey and Maneesh Agrawala, 2006. "Multi-Scale Banking to 45"
 ##' IEEE Transactions On Visualization And Computer Graphics.
 ##'
-##' @inheritParams slopes
-##' @return \code{numeric} The aspect ratio.
-##'
-banking_ms <- function(x, y, cull=FALSE) {
-    s <- abs(slopes(x, y, cull=cull))
-    Rx <- diff(range(x))
-    Ry <- diff(range(y))
-    median(s) * Rx / Ry
-}
-
-##' Average-Absolute-Orientation Banking
-##'
-##' Find the aspect ratio by setting the average orientation to 45
-##' degrees. For aspect ratio \deqn{\alpha}{alpha}, let the
-##' orientation of a line segment be \deqn{\theta_i(\alpha) = atan(s_i
-##' / \alpha)}.
-##'
-##' \eqn{
-##' \frac{ \sum_i \theta_i(\alpha) l_i}{\sum_i l_i} = \frac{\pi}{4} rad
-##' }
-##' where \deqn{l_i = 1} if unweighted, and \deqn{l_i = \sqrt{x_i^2 + y_i^2}}
-##' (length of the line segment), if weighted.
-##' The value of \deqn{\alpha} is found with \code{\link{nlm}}.
-##'
-##' @inheritParams banking_mas
-##'
-##' @return \code{numeric} The aspect ratio.
-##' @references
 ##' Cleveland, W. S. 1993. "A Model for Studying Display Methods of Statistical
 ##' Graphs." Journal of Computational and Statistical Graphics.
 ##'
 ##' Cleveland, W. S. 1994. The Elements of Graphing Data, Revised Edition.
 ##'
-##' Heer, Jeffrey and Maneesh Agrawala, 2006. "Multi-Scale Banking to 45"
-##' IEEE Transactions On Visualization And Computer Graphics.
-banking_ao <- function(x, y, weight=FALSE, cull=FALSE, ...) {
-    s <- slopes(x, y, cull=cull)
-    alpha0 <- banking_ms(x, y, cull=cull)
+##' @return \code{numeric} The aspect ratio.
+##'
+##' @export
+##' @examples
+##' x <- seq_along(sunspot.year)
+##' y <- as.numeric(sunspot.year)
+##' ## Using the default method, Median Absolute Slope
+##' bank_slopes(x, y)
+##' bank_slopes(x, y, method="ms")
+##' ## Using culling
+##' bank_slopes(x, y, method="ms", cull=TRUE)
+##' ## Average Absolute Slope
+##' bank_slopes(x, y, method="as")
+##' bank_slopes(x, y, method="as", cull=TRUE)
+##' ## Average Orientation
+##' bank_slopes(x, y, method="ao")
+##' bank_slopes(x, y, method="ao", cull=TRUE)
+##' ## Average Orientation (Weighted)
+##' bank_slopes(x, y, method="ao", weight=TRUE)
+##' bank_slopes(x, y, method="ao", cull=TRUE, weight=TRUE)
+##' ## Global Orientation Resolution
+##' bank_slopes(x, y, method="gor")
+##' bank_slopes(x, y, method="gor", cull=TRUE)
+##' ## Local Orientation Resolution
+##' bank_slopes(x, y, method="lor")
+##' bank_slopes(x, y, method="lor", cull=TRUE)
+bank_slopes <- function(x, y, cull=FALSE, method="ms", weight=TRUE, ...) {
+    FUN <- get(sprintf("bank_slopes_%s", method))
+    FUN(slopes(x, y, cull=TRUE), weight=weight, ...)
+}
+
+bank_slopes_ms <- function(slopes, ...) {
+    Rx <- diff(slopes$range_x)
+    Ry <- diff(slopes$range_y)
+    median(abs(slopes$s)) * Rx / Ry
+}
+
+bank_slopes_as <- function(slopes, ...) {
+    Rx <- diff(slopes$range_x)
+    Ry <- diff(slopes$range_y)
+    mean(abs(slopes$s)) * Rx / Ry
+}
+
+bank_slopes_ao <- function(slopes, weight=TRUE, ...) {
+    s <- slopes$s
+    alpha0 <- bank_slopes_ms(slopes)
     if (weight) {
-        w <- attr(s, "lengths")
+        f <- function(alpha) {
+            (weighted.mean(abs(atan(s / alpha)), slopes$length) - FORTY_FIVE)^2
+        }
     } else {
-        w <- rep(1, length(s))
+        f <- function(alpha) {
+            (mean(abs(atan(s / alpha))) - FORTY_FIVE)^2
+        }
     }
-    f <- function(alpha) {
-        (weighted.mean(abs(atan(s / alpha)), w) - FORTY_FIVE)^2
-        ##(mean(abs(atan(s / alpha))) - FORTY_FIVE)^2
-    }
-    nlm(f, alpha0, fscale=0, ...)$estimate
-    #optimize(f, lower=lower, upper=upper, ...)
+    nlm(f, alpha0, fscale=0)$estimate
 }
 
-##' Average Absolute Slope Banking
-##'
-##' Let the aspect ratio be \deqn{\alpha = \frac{w}{h}}{alpha = w/h}.
-##' then the mean absolute slope banking is the
-##' \deqn{\alpha}{alpha} such that,
-##' \eqn{
-##'   mean \left| \frac{s_i}{\alpha} \right| = 1
-##' }{
-##'  mean |s_i / alpha| = 1
-##' }
-##'
-##' Let \deqn{R_z = z_{max} - z_{min}} for \deqn{z = x, y},
-##' and \eqn{M = mean \| s_i \|}. Then,
-##' \eqn{
-##' \alpha = M R_x / R_y
-##' }
-##'
-##' @references
-##' Heer, Jeffrey and Maneesh Agrawala, 2006. "Multi-Scale Banking to 45"
-##' IEEE Transactions On Visualization And Computer Graphics.
-##'
-banking_as <- function(x, y, cull=FALSE) {
-    s <- abs(slopes(x, y, cull=cull))
-    Rx <- diff(range(x))
-    Ry <- diff(range(y))
-    mean(s) * Rx / Ry
-}
-
-##' Banking by Optimizing Orientation Resolution
-##'
-##' The angle between line segments i and j is
-##' \eqn{r_{i,j} = \|\theta_i(\alpha) - \theta_j(\alpha)\|}
-##'
-##' where \deqn{\theta_i(\alpha) = atan(s_i /  \alpha)} and
-##' \deqn{s_i} is the slope of line segment i.
-##' This function finds the  \deqn{\alpha} that maximizes
-##' the sum of the angles between all pairs of line segments.
-##'
-##' \eqn{
-##'   \max_{\alpha} \sum_i \sum_{j: j < 1} r_{i,j}
-##' }
-##'
-##' The local optimization only includes line-segments
-##' that are next to each other. Suppose there are n line
-##' segments, then the local orientation orientation
-##' has the following objective function.
-##'
-##' \eqn{
-##'   \max_{\alpha} \sum_{i=2}^{n} r_{i,i-1}
-##' }
-##'
-##' @references
-##' Heer, Jeffrey and Maneesh Agrawala, 2006. "Multi-Scale Banking to 45"
-##' IEEE Transactions On Visualization And Computer Graphics.
-banking_gor <- function(x, y, cull=FALSE, ...) {
-    alpha0 <- banking_ms(x, y, cull=cull)
-    s <- slopes(x, y, cull=cull)
+bank_slopes_gor <- function(slopes, ...) {
+    alpha0 <- bank_slopes_ms(slopes)
+    s <- slopes$s
     f <- function(alpha) {
         theta <- atan(s / alpha)
         -1 * mean(as.numeric(dist(theta, method="manhattan")))
     }
-    nlm(f, alpha0, ...)$estimate
+    nlm(f, alpha0)$estimate
 }
 
-banking_lor <- function(x, y, cull=FALSE, ...) {
-    alpha0 <- banking_ms(x, y, cull=cull)
-    s <- slopes(x, y, cull=cull)
+bank_slopes_lor <- function(slopes, ...) {
+    alpha0 <- bank_slopes_ms(slopes)
+    s <- slopes$s
     f <- function(alpha) {
         -1 * mean(abs(diff(atan(s / alpha))))
     }
-    nlm(f, alpha0, ...)$estimate
+    nlm(f, alpha0)$estimate
 }
 
 
