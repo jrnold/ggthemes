fcb2063953f02a99d1dfe75fd82db526176fd9aa
Håkon Malmedal <hmalmedal@gmail.com>
2014-07-06 14:28:29 +0200

    typos

--- R/ggthemes-package.R
+++ R/ggthemes-package.R
@@ -3,7 +3,7 @@
 ##' This package contains extra themes, scales, and geoms, and
 ##' functions for and related to \pkg{ggplot2}.
 ##'
-##' In addtion to the help pages, see the REAMDE page on
+##' In addition to the help pages, see the README page on
 ##' \href{https://github.com/jrnold/ggthemes}{github} for examples.
 ##'
 ##' @name ggthemes
