1817e8d50b874c36e1bab06c2dacf3c89663f742
Gergely Daróczi <daroczig@rapporter.net>
2014-09-21 13:57:30 +0200

    add support for font family/color/size

--- R/pander.R
+++ R/pander.R
@@ -2,6 +2,9 @@
 #'
 #' The \pkg{pander} ships with a default theme when the "unify plots" option is enabled via \code{panderOptions}, which is now also available outside of \pkg{pander} internals, like \code{evals}, \code{eval.msgs} or \code{Pandoc.brew}.
 #' @param nomargin suppress the white space around the plot (boolean)
+#' @param ff font family, like \code{sans}
+#' @param fc font color (name or hexa code)
+#' @param fs font size (integer)
 #' @param gM major grid (boolean)
 #' @param gm minor grid (boolean)
 #' @param gc grid color (name or hexa code)
@@ -14,7 +17,7 @@
 #' panderOptions('graph.grid.color', 'red')
 #' p + theme_pander()
 #' }
-theme_pander <- function(nomargin = TRUE, gM = TRUE, gm = TRUE, gc = 'grey', gl = 'dashed') {
+theme_pander <- function(nomargin = TRUE, ff = 'sans', fc = 'black', fs = 12, gM = TRUE, gm = TRUE, gc = 'grey', gl = 'dashed') {
 
     ## load currently stored values from `panderOptions` if available
     if (require(pander)) {
@@ -30,11 +33,21 @@ theme_pander <- function(nomargin = TRUE, gM = TRUE, gm = TRUE, gc = 'grey', gl 
             gl <- panderOptions('graph.grid.lty')
     }
 
-    ## default colors
+    ## default colors and font
     res <- theme(
         panel.grid       = element_line(colour = gc, size = 0.2, linetype = gl),
         panel.grid.major = element_line(colour = gc, size = 0.2, linetype = gl),
-        panel.grid.minor = element_line(colour = gc, size = 0.1, linetype = gl)
+        panel.grid.minor = element_line(colour = gc, size = 0.1, linetype = gl),
+        plot.title       = element_text(colour = fc, family = ff, face = "bold", size = fs * 1.2),
+        axis.text        = element_text(colour = fc, family = ff, face = 'plain', size = fs * 0.8),
+        axis.text.x      = element_text(colour = fc, family = ff, face = 'plain', size = fs * 0.8),
+        axis.text.y      = element_text(colour = fc, family = ff, face = 'plain', size = fs * 0.8),
+        legend.text      = element_text(colour = fc, family = ff, face = 'plain', size = fs * 0.8),
+        legend.title     = element_text(colour = fc, family = ff, face = 'italic', size = fs),
+        axis.title.x     = element_text(colour = fc, family = ff, face = 'plain', size = fs),
+        strip.text.x     = element_text(colour = fc, family = ff, face = 'plain', size = fs),
+        axis.title.y     = element_text(colour = fc, family = ff, face = 'plain', size = fs, angle = 90),
+        strip.text.y     = element_text(colour = fc, family = ff, face = 'plain', size = fs, angle = -90)
     )
 
     ## disable grid
