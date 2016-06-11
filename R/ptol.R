cc1d0991fcaaf195385abd16c98ec39ccb393a35
jrnold <jeffrey.arnold@gmail.com>
2016-06-10 20:27:57 -0700

    Update ptol_pal and ptol_scale
    - remove n argument from `ptol_pal`. The function that `ptol_pal` takes the n argument anyways.
    - update `scale_colour_ptol` and `scale_fill_ptol` based on changes to `ptol_pal`. They not longer take the `n` argument.
    - edit ggthemes_data$ptol

--- R/ptol.R
+++ R/ptol.R
@@ -1,31 +1,30 @@
-#' Color Palletes from Paul Tol's "Colour Schemes
+#' Color Palettes from Paul Tol's "Colour Schemes"
 #'
 #' Qualitative color palettes from Paul Tol,
 #' \href{https://personal.sron.nl/~pault/colourschemes.pdf}{"Colour Schemes"}.
+#'
 #' Incorporation of the palette into an R package was originally inspired by
 #' Peter Carl's [Paul Tol 21 Gun Salute](https://tradeblotter.wordpress.com/2013/02/28/the-paul-tol-21-color-salute/)
 #'
 #' @export
-#' @param n number of colors; integer value between 1 and 12
 #' @family colour ptol
+#' @references
+#' Paul Tol. 2012. "Colour Schemes." SRON Technical Note, SRON/EPS/TN/09-002.
+#'  \url{https://personal.sron.nl/~pault/colourschemes.pdf}
 #' @examples
 #' library("scales")
 #' show_col(ptol_pal()(6))
-#' show_col(ptol_pal(4)(4))
-#' show_col(ptol_pal(4)(12))
-ptol_pal <- function(n) {
-  if (missing(n)) {
-    n <- 6
+#' show_col(ptol_pal()(4))
+#' show_col(ptol_pal()(12))
+ptol_pal <- function() {
+  function(n) {
+    if (n > 12) {
+      stop(sprintf("%s is greater than the max number of colors", n))
+    } else if (n < 1) {
+      stop("the number of colors cannot be zero")
+    }
+    ggthemes_data$ptol$qualitative[[n]]
   }
-  else if (n > 12) {
-    stop(sprintf("%s is greater than the max number of colors", n))
-  }
-  else if (n == 0) {
-    stop("the number of colors cannot be zero")
-  }
-    values <- ggthemes_data$ptol[[n]]
-    l <- length(values)
-    scales::manual_pal(unname(values[1:l]))
 }
 
 #' Color Scales from Paul Tol's "Colour Schemes
@@ -37,8 +36,8 @@ ptol_pal <- function(n) {
 #' @family colour ptol
 #' @rdname scale_ptol
 #' @export
-scale_colour_ptol <- function(n, ...) {
-    discrete_scale("colour", "ptol", ptol_pal(n), ...)
+scale_colour_ptol <- function(...) {
+  discrete_scale("colour", "ptol", ptol_pal(), ...)
 }
 
 #' @export
@@ -47,6 +46,6 @@ scale_color_ptol <- scale_colour_ptol
 
 #' @export
 #' @rdname scale_ptol
-scale_fill_ptol <- function(n, ...) {
-    discrete_scale("fill", "ptol", ptol_pal(n), ...)
+scale_fill_ptol <- function(...) {
+  discrete_scale("fill", "ptol", ptol_pal(), ...)
 }
