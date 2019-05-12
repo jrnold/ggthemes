388755323b45611f0f2e748944ecd8c37c9a3d1f
jrnold <jeffrey.arnold@gmail.com>
2019-05-12 14:31:39 -0700

    Make example require library conditional

--- inst/examples/ex-scale_linetype_stata.R
+++ inst/examples/ex-scale_linetype_stata.R
@@ -1,14 +1,13 @@
-require("dplyr")
-require("tidyr")
 require("ggplot2")
+if (require("tidyr") && require("dplyr")) {
+  rescale01 <- function(x) {
+    (x - min(x)) / diff(range(x))
+  }
 
-rescale01 <- function(x) {
-  (x - min(x)) / diff(range(x))
+  gather(economics, variable, value, -date) %>%
+    group_by(variable) %>%
+    mutate(value = rescale01(value)) %>%
+    ggplot(aes(x = date, y = value, linetype = variable)) +
+    geom_line() +
+    scale_linetype_stata()
 }
-
-gather(economics, variable, value, -date) %>%
-  group_by(variable) %>%
-  mutate(value = rescale01(value)) %>%
-  ggplot(aes(x = date, y = value, linetype = variable)) +
-  geom_line() +
-  scale_linetype_stata()
--- inst/examples/ex-theme_pander.R
+++ inst/examples/ex-theme_pander.R
@@ -1,18 +1,19 @@
 require("ggplot2")
-require("pander")
+if (require("pander")) {
 
-p <- ggplot(mtcars, aes(x = mpg, y = wt)) +
-     geom_point()
-p + theme_pander()
+  p <- ggplot(mtcars, aes(x = mpg, y = wt)) +
+       geom_point()
+  p + theme_pander()
 
-panderOptions("graph.grid.color", "red")
-p + theme_pander()
+  panderOptions("graph.grid.color", "red")
+  p + theme_pander()
 
-p <- ggplot(mtcars, aes(wt, mpg, colour = factor(cyl))) +
-  geom_point()
-p + theme_pander() + scale_color_pander()
+  p <- ggplot(mtcars, aes(wt, mpg, colour = factor(cyl))) +
+    geom_point()
+  p + theme_pander() + scale_color_pander()
 
-ggplot(mpg, aes(x = class, fill = drv)) +
-  geom_bar() +
-  scale_fill_pander() +
-  theme_pander()
+  ggplot(mpg, aes(x = class, fill = drv)) +
+    geom_bar() +
+    scale_fill_pander() +
+    theme_pander()
+}
