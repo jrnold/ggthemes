fe28c39df374cf9276c0fa374f43d15557149aa1
Jeffrey Arnold <jeffrey.arnold@instacart.com>
2020-12-27 20:48:24 -0800

    Fix #124
    Conditionally calls vdiffr in tests.

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -1,20 +1,10 @@
-enable_vdiffr <- TRUE
-
-# if (!requireNamespace("vdiffr", quietly = TRUE) ||
-#     utils::packageVersion("vdiffr") < "0.2.3.9000") {
-#   enable_vdiffr <- FALSE
-# }
-
 expect_doppelganger <- function(title, fig,
                                 path = NULL,
                                 ...,
                                 user_fonts = NULL,
                                 verbose = FALSE) {
-  if (!enable_vdiffr) {
-    # expect_error(regexp = NA, ggplot_build(fig))
-    return(invisible(NULL))
-  }
-
+  # need to call conditionally because vdiffr listed in Suggests (#124)
+  testthat::skip_if_not_installed("vdiffr")
   vdiffr::expect_doppelganger(title, fig,
                               path = path,
                               ...,
