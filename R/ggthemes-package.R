ed5ca82e60fb5356d641092c7ddd07c1b80b787c
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2024-02-07 21:38:06 -0800

    Update package file
    Needed to make changes to fix this error from
    `pkgdown::build_site()`:
    
    ```
    ! All topics must be included in reference index
    ✖ Missing topics: ggthemes
    ℹ Either add to _pkgdown.yml or use @keywords internal
    ℹ See `$stdout` for standard output.
    ```
    
    Followed the lead of the ggplot2 package:
    
    - add `@keywords internal`
    - Let roxygen2 autogenerate the entire package page.

--- R/ggthemes-package.R
+++ R/ggthemes-package.R
@@ -1,17 +1,12 @@
-#' ggthemes
-#'
-#' This package contains extra themes, scales, and geoms, and
-#' functions for and related to \pkg{ggplot2}. See
-#' \url{https://jrnold.github.io/ggthemes/} for documentation.
-#'
-#' @name ggthemes
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
 #' @importFrom ggplot2 ggplot theme ggproto aes draw_key_path
 #' @importFrom grid grobTree grobName gTree gList segmentsGrob gpar
 #' @importFrom scales manual_pal div_gradient_pal seq_gradient_pal
 #' @importFrom graphics abline axis text points
 #' @importFrom methods hasArg as
 #' @importFrom graphics par
-"_PACKAGE"
+NULL
 
 globalVariables(".data")
