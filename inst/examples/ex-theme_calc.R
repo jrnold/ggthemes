37cd86549a5efca5867e4b59167af7b1acf03326
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2024-02-08 17:44:19 -0800

    Fix calc doc example errors

--- inst/examples/ex-theme_calc.R
+++ inst/examples/ex-theme_calc.R
@@ -1,11 +1,14 @@
 library("ggplot2")
 
-p <- ggplot(mtcars) +
-     geom_point(aes(x = wt, y = mpg, colour = factor(gear))) +
-     facet_wrap(~am) + theme_calc()
-p + scale_color_calc()
-q <- ggplot(mtcars) +
-     geom_point(aes(x = wt, y = mpg, shape = factor(gear))) +
-     facet_wrap(~am) +
-     theme_calc()
-q + scale_shape_calc()
+ggplot(mtcars) +
+  geom_point(aes(x = wt, y = mpg, colour = factor(gear))) +
+  facet_wrap(~am) +
+  theme_calc() +
+  scale_color_calc()
+\dontrun{
+ggplot(mtcars) +
+  geom_point(aes(x = wt, y = mpg, shape = factor(gear))) +
+  facet_wrap(~am) +
+  theme_calc() +
+  scale_shape_calc()
+}
