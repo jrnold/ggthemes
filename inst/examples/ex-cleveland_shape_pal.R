a3107ef7f378ffd505cc41e91173b97263067202
Jeffrey Arnold <jeffrey.arnold@gmail.com>
2024-02-08 16:16:25 -0800

    Fix doc example error

--- inst/examples/ex-cleveland_shape_pal.R
+++ inst/examples/ex-cleveland_shape_pal.R
@@ -1,6 +1,6 @@
 ###   (discrete).
 
-
+\dontrun{
 library("ggplot2")
 p <- ggplot(mtcars) +
      geom_point(aes(x = wt, y = mpg, shape = factor(gear))) +
@@ -10,3 +10,4 @@ p <- ggplot(mtcars) +
 p + scale_shape_cleveland()
 # non-overlapping symbol palette
 p + scale_shape_cleveland(overlap = FALSE)
+}
