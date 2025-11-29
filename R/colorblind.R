27263cfd1e5f1ab50df7d16eb64396a9a492b128
Jeff Arnold <jeffrey.arnold@gmail.com>
2025-11-29 12:43:24 -0800

    [feat] Add aliases for British spelling of colour
    - Introduced 'colourblind_pal' as an alias for 'colorblind_pal'.
    - Added 'scale_colour_colourblind' and 'scale_fill_colourblind' functions to complement existing colorblind scales.
    
    From https://github.com/jrnold/ggthemes/pull/180
    
    Authored by @hangonasecond

--- R/colorblind.R
+++ R/colorblind.R
@@ -23,16 +23,24 @@ colorblind_pal <- function() {
 
 #' @rdname colorblind
 #' @export
+colourblind_pal <- colorblind_pal
+
+#' @rdname colorblind
+#' @export
 scale_colour_colorblind <- function(...) {
   discrete_scale("colour", palette = colorblind_pal(), ...)
 }
 
 #' @rdname colorblind
 #' @export
-scale_color_colorblind <- scale_colour_colorblind
+scale_colour_colourblind <- scale_colour_colorblind
 
 #' @rdname colorblind
 #' @export
 scale_fill_colorblind <- function(...) {
   discrete_scale("fill", palette = colorblind_pal(), ...)
 }
+
+#' @rdname colorblind
+#' @export
+scale_fill_colourblind <- scale_fill_colorblind
